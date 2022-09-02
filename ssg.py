7bb0a89513ee21818ed1e4f880879777c5862f2d
Philip Wrage <philipwrage@users.noreply.github.com>
2022-09-01 22:37:17 -0500

    Create Site class and invoke Typer to build site

---
+++ ssg.py
@@ -0,0 +1,12 @@
+import typer
+from ssg.site import Site
+
+
+def main(source: str = "content", dest: str = "dist"):
+    config = {
+        "source": source,
+        "dest": dest
+    }
+    Site(**config).build()
+
+typer.run(main)
